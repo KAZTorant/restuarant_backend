4b626b967c63b61955ceefc679efbd3436c57de6
Zakir Aghayev <zakir-aghayev@outlook.com>
2025-05-02 15:45:58 +0400

    printadded

--- apps/printers/apis.py
+++ apps/printers/apis.py
@@ -12,17 +12,20 @@ class PrintCheckAPIView(APIView):
     permission_classes = [IsAuthenticated, AtMostAdmin]
 
     def post(self, request, table_id):
+        print("Print", table_id)
         if not table_id:
             return Response({"error": "Table ID is required."}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
             printer = PrinterService()
             success, message = printer.print_orders_for_table(table_id)
+            print("Print", success, message)
             if success:
                 return Response({"message": message}, status=status.HTTP_200_OK)
             else:
                 return Response({"error": message}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
+            print("Print", e)
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def delete(self, request, table_id):
