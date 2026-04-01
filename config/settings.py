7823314f32b8339e1bc555e55fb3408d09473b9d
Aykhan1 <Aykhan.Alifov@azai.tech>
2026-04-01 22:31:25 +0400

    chore: update database configuration for restaurant_db

--- config/settings.py
+++ config/settings.py
@@ -114,15 +114,15 @@ DATABASES = {
 }
 if DB_DEFAULT:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'mydatabasenew',
-            'USER': 'myusernew',
-            'PASSWORD': 'adminadmin',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restaurant_db',
+        'USER': 'aykhan',
+        'PASSWORD': 'your_password',  # Use the password you set
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
