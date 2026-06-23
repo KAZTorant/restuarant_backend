478ac51c106fe588dd1c7902b07758420f9958d9
Kamran Hacili <khajili98@gmail.com>
2026-06-23 21:22:10 +0400

    fix: merge conflicting payments migrations

---
+++ apps/payments/migrations/0007_merge.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0006_alter_paymentcalculation_total_amount'),
+        ('payments', '0006_restaurant_required'),
+    ]
+
+    operations = []
